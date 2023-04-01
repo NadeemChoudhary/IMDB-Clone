67a8966670ec75eaf82a799999930d7ce5aec844
NadeemChoudhary <nadeemchoudhary37@gmail.com>
2023-04-01 16:13:24 +0530

    remove  ' error

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,48 +2,48 @@ import React from "react";
 
 export default function About() {
     return (
-      <div className="max-w-6xl mx-auto space-y-4 py-6">
+      <div className="max-w-6xl mx-auto space-y-4 py-6 text-slate-50">
         <h2>About Us</h2>
         <p className="mb-4">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
+          industry. Lorem Ipsum has been the industry s standard dummy text ever
+          since the 1500s when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry's standard
+          typesetting industry. Lorem Ipsum has been the industry s standard
           dummy text ever since the 1500s, when an unknown printer took a galley
           of type and scrambled it to make a type specimen book. It has survived
           not only five centuries
         </p>
         <p className="mb-4">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry's standard
+          typesetting industry. Lorem Ipsum has been the industry s standard
           dummy text ever since the 1500s, when an unknown printer took a galley
           of type and scrambled it to make a type specimen book. It has survived
           not only five centuries
         </p>
         <p className="mb-4">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry's standard
+          typesetting industry. Lorem Ipsum has been the industry s standard
           dummy text ever since the 1500s, when an unknown printer took a galley
           of type and scrambled it to make a type specimen book. It has survived
           not only five centuries
         </p>
         <p className="mb-4">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry's standard
+          typesetting industry. Lorem Ipsum has been the industry s standard
           dummy text ever since the 1500s, when an unknown printer took a galley
           of type and scrambled it to make a type specimen book. It has survived
           not only five centuries
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,19 @@
 "use client"
-
 import Cards from '@/Components/Cards';
 import axios from 'axios';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { flushSync } from 'react-dom';
 export default function Home() {
-  const key = process.env.API
   const { get } = useSearchParams();
   const category = get('gener');
   console.log(category)
-  const [Data, setData] = useState([])
+  const [Data, setData] = useState([]);
   const FetchData = async () => {
     try {
       const { data: { results } } = await axios.get(`https://api.themoviedb.org/3/movie/${category}?api_key=1cb845cc55dd460ae0d5f6c75731b7c3&language=en-US&page=1`);
-      if(results){
+      if (results) {
         setData(results)
       }
-
     } catch (error) {
       alert(error)
     }
